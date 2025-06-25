652f06e66c85c5d9d56d9e28685e2a36a5b4c757
roaarafat <roaabasheergmail.com>
2025-06-25 17:28:30 +0300

    إضافة ملفات المهام وتعديل ملف المسارات

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    $name = "roaa";
+    $departments = [
+        '01' => 'Tichnical',
+        '02' => 'Financial',
+        '03' => 'Sales'
+    ];
+    return view('about', compact('name', 'departments'));
+});
+
+Route::post('/about', function () {
+    $name = request('name');
+    $departments = [
+        '01' => 'Tichnical',
+        '02' => 'Financial',
+        '03' => 'Sales'
+    ];
+    return view('about', compact('name', 'departments'));
+});
+
+Route::get('tasks', [TaskController::class , 'index']);
+
+Route::post('create' , [TaskController::class , 'create']);
+
+Route::post('delete/{id}' , [TaskController::class , 'destroy'] );
+
+Route::post('edit/{id}', [TaskController::class , 'edit']);
+
+Route::post('update', [TaskController::class , 'update']);
+
